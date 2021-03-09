8b31d9088cd53365592285edb8c0698ed0b0e48f
Scott Brabson <scottbrabson@gmail.com>
2021-03-09 14:43:28 -0700

    add count method and notes

--- index.js
+++ index.js
@@ -53,17 +53,21 @@ async function getCourses() {
     // .or([{author: 'Scott'}, {isPublished: true}]) // array of filter objects
     // .and([]) // array of filter objects
     
+    
     //            REGEX
     // /^string/ - finds all string that start with Scott
-    .find({ author: /^Scott/ }) 
+    // .find({ author: /^Scott/ }) 
     // /pattern$/ - finds strings that end in Brabson - 'i' means case INsensitive
-    .find({ author: /Brabson$/i }) 
+    // .find({ author: /Brabson$/i }) 
     // contains Scott
-    .find({ author: /.*Scott.*/i }) // can have 0 or more characters before or at the end of 'scott'
+    // .find({ author: /.*Scott.*/i }) // can have 0 or more characters before or at the end of 'scott'
 
     .limit(10)
     .sort({name: 1}) // a => b, -1 is b=> a
     .select({ name: 1, tags: 1}) // what props to return from those objects
+    
+    // .count()  // will return # of items that match filter criteria 
+    .skip()
   console.log(courses);
 }
 
