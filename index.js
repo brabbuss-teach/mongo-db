5f4106ac4d0b837b7e55b843eb818d5ac80f32f1
Scott Brabson <scottbrabson@gmail.com>
2021-03-09 14:55:42 -0700

    add mock data to db

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ const courseSchema = new mongoose.Schema({
   isPublished: Boolean,
 });
 
-// mongoose.model() - 2 args,
-// 1 - singular name of the collection this model is for
-// 2 - schema that defines the shape of docs in this collection
 const Course = mongoose.model("Course", courseSchema); // essentially a Class! PascalCase
 
 async function createCourse() {
@@ -31,43 +28,18 @@ async function createCourse() {
 }
 
 async function getCourses() {
-  // const courses = await Course.find() // return all
-
-  // eq equal
-  // ne not equal
-  // gt greater than
-  // gte greater than or equal to
-  // lt less than
-  // lte less than or equal to
-  // in
-  // nin not in
-  // or
-  // and
+  const pageNumber = 2;
+  const pageSize = 10;
+  // /api/courses?pageNumber=2&pageSize=10
 
   const courses = await Course
-    // .find({author: 'Scott', isPublished: true}) // filter with arg object
-    // .find({price: { $gte: 10, $lte: 20 } })
-    // .find({price: {$in: [10, 15, 20]}})  // returns obj w/price === 10, 15, or 20
+    .find({author: 'Scott', isPublished: true})
     
-    // .find()
-    // .or([{author: 'Scott'}, {isPublished: true}]) // array of filter objects
-    // .and([]) // array of filter objects
-    
-    
-    //            REGEX
-    // /^string/ - finds all string that start with Scott
-    // .find({ author: /^Scott/ }) 
-    // /pattern$/ - finds strings that end in Brabson - 'i' means case INsensitive
-    // .find({ author: /Brabson$/i }) 
-    // contains Scott
-    // .find({ author: /.*Scott.*/i }) // can have 0 or more characters before or at the end of 'scott'
+    .skip((pageNumber - 1) * pageSize)  // allows for pagination with these two fxns
+    .limit(pageSize)  // this would be grabbed from the req.params
 
-    .limit(10)
     .sort({name: 1}) // a => b, -1 is b=> a
     .select({ name: 1, tags: 1}) // what props to return from those objects
-    
-    // .count()  // will return # of items that match filter criteria 
-    .skip()
   console.log(courses);
 }
 
